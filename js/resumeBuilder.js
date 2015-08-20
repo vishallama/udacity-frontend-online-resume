04c8137f111e4548ef70526bd93e557736f980ec
Vishal Lama <contact@vishallama.org>
2015-08-20 12:24:32 -0600

    Add description for js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,10 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
 
+This file contains all of the code needed to create four JSONs corresponding
+to four distinct sections in index.html: work, education, projects and a
+header with biographical information.
+
+In addition, this file also uses code from helper.js to render index.html
+correctly.
+
+ */
