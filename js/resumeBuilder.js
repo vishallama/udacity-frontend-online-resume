7103219ed8931392323b6260779bcebedbb3a306
Vishal Lama <contact@vishallama.org>
2015-08-20 12:40:22 -0600

    Add JSON object bio containing biographical info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,3 +8,23 @@ In addition, this file also uses code from helper.js to render index.html
 correctly.
 
  */
+
+/* JSON object containing biographical info */
+var bio = {
+    name: "Vishal Lama",
+    role: "Web Developer",
+    contacts: {
+        mobile: "123-456-7890",
+        email: "contact@vishallama.org",
+        github: "vishallama",
+        twitter: "@vishallama",
+        location: "Cedar City, UT"
+    },
+    welcomeMessage: "Welcome!",
+    skills: ["Python", "JavaScript", "HTML/CSS", "Bootstrap", "Linux"],
+    biopic: "images/vishal.png",
+    display: function() {
+
+    }
+}
+
