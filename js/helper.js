57ede0167cbefc911364303e328725cca4cfe673
Vishal Lama <contact@vishallama.org>
2015-08-22 11:11:02 -0600

    Implement click()

--- js/helper.js
+++ js/helper.js
@@ -93,7 +93,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+    // your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
 });
 
 
