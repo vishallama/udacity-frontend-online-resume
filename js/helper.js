74f2655efb9ebc2fce71fef4e361f20c7d7bfa18
Vishal Lama <contact@vishallama.org>
2015-08-28 13:25:00 -0600

    Disable map scrolling, and fix indentation

--- js/helper.js
+++ js/helper.js
@@ -121,8 +121,7 @@ $(document).click(function(loc) {
 
 /*
 This is the fun part. Here's where we generate the custom Google Map
-for the website.
-See the documentation below for more details.
+for the website. See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
 var map;    // declares a global map variable
@@ -137,7 +136,8 @@ function initializeMap() {
   var mapOptions = {
     disableDefaultUI: true,
     zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false
   };
 
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
